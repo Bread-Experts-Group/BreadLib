d1561711848e412f301c8cbb65206d6e24a279cc
ATPStorages <tcgytsc@gmail.com>
2026-07-03 15:36:45 -0500

    Idea

--- common/src/main/java/org/bread_experts_group/breadlib/network/payload/PayloadInfo.java
+++ common/src/main/java/org/bread_experts_group/breadlib/network/payload/PayloadInfo.java
@@ -1,13 +1,14 @@
 package org.bread_experts_group.breadlib.network.payload;
 
+import io.netty.buffer.ByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import org.bread_experts_group.breadlib.network.NetworkDirection;
 
-public record PayloadInfo<T extends CustomPacketPayload>(
-		PayloadHandler<?> handler,
+public record PayloadInfo <B extends ByteBuf, T extends CustomPacketPayload>(
+		PayloadHandler<T> handler,
 		CustomPacketPayload.Type<T> type,
-		StreamCodec<?, T> streamCodec,
+		StreamCodec<B, T> streamCodec,
 		NetworkDirection bound
 ) {
 }
