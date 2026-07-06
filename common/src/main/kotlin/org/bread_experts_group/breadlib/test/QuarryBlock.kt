960e06c8d97532e9b611fc02ff9ff7ad3dee760d
ATPStorages <tcgytsc@gmail.com>
2026-07-06 10:32:22 -0500

    Quarry logic

--- common/src/main/kotlin/org/bread_experts_group/breadlib/test/QuarryBlock.kt
+++ common/src/main/kotlin/org/bread_experts_group/breadlib/test/QuarryBlock.kt
@@ -1,9 +1,11 @@
 package org.bread_experts_group.breadlib.test
 
+import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.HorizontalDirectionalBlock
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties
+import net.minecraft.world.level.block.state.BlockState
 import org.bread_experts_group.breadlib.extensions.block.BlockProperties
 import org.bread_experts_group.breadlib.extensions.block.BreadLibBlockWithEntity
 
@@ -16,4 +18,19 @@ class QuarryBlock : BreadLibBlockWithEntity<QuarryBlockEntity>(
 	serverTick = QuarryBlockEntity::tick
 ) {
 	override fun breadLibProperties(): BlockProperties = blockProperties
+
+	override fun onRemove(
+		state: BlockState,
+		level: Level,
+		pos: BlockPos,
+		newState: BlockState,
+		movedByPiston: Boolean
+	) {
+		(level.getBlockEntity(pos) as? QuarryBlockEntity)?.let {
+			val id = it.id ?: return@let
+			val drillPos = it.drillPos ?: return@let
+			level.destroyBlockProgress(id, drillPos, -1)
+		}
+		super.onRemove(state, level, pos, newState, movedByPiston)
+	}
 }
--- common/src/main/kotlin/org/bread_experts_group/breadlib/test/QuarryBlockEntity.kt
+++ common/src/main/kotlin/org/bread_experts_group/breadlib/test/QuarryBlockEntity.kt
@@ -1,36 +1,32 @@
 package org.bread_experts_group.breadlib.test
 
-import net.minecraft.client.multiplayer.ClientLevel
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.core.HolderLookup
 import net.minecraft.core.component.DataComponents
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
-import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.item.ItemEntity
-import net.minecraft.world.entity.monster.Skeleton
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
-import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.HorizontalDirectionalBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.level.material.FluidState
-import net.minecraft.world.level.material.Fluids
+import net.minecraft.world.level.saveddata.SavedData
 import kotlin.math.ceil
 import kotlin.math.roundToInt
 
 class QuarryBlockEntity(pPos: BlockPos, pBlockState: BlockState) : BlockEntity(
 	BlockEntityTypeTest.QUARRY_TYPE.get(), pPos, pBlockState
 ) {
-	var x = -10
-	var z = -10
-
-	var y = blockPos.y - 1
-
 	val xMine = 10
 	val zMine = 10
+
+	var x = -xMine
+	var z = -zMine
+
+	var y = blockPos.y - 1
 
 	var noUpdate = false
 
@@ -42,30 +38,39 @@ class QuarryBlockEntity(pPos: BlockPos, pBlockState: BlockState) : BlockEntity(
 	var breakingTick = 0
 	var tickTime = 0f
 
-	private var id: Int? = null
+	var id: Int? = null
 
 	fun tick(level: ServerLevel, pos: BlockPos, state: BlockState) {
-		if (id == null) id = ItemEntity(level, 0.0, 0.0, 0.0, ItemStack(this.blockState.block)).id
+		if (id == null) id = ItemEntity(level, 0.0, 0.0, 0.0, ItemStack(this.blockState.block)).let {
+			it.discard()
+			it.id
+		}
 		if (drillPos == null) {
-			val (nX, nZ) = when (state.getValue(HorizontalDirectionalBlock.FACING)) {
+			val facing = state.getValue(HorizontalDirectionalBlock.FACING).opposite
+			val (nX, nZ) = when (facing) {
 				Direction.UP -> 0 to 0
 				Direction.DOWN -> 0 to 0
-				Direction.NORTH -> 1 to -1
-				Direction.SOUTH -> -1 to 1
-				Direction.WEST -> -1 to 1
-				Direction.EAST -> 1 to -1
+				Direction.NORTH -> -1 to 1
+				Direction.SOUTH -> 1 to -1
+				Direction.WEST -> 1 to -1
+				Direction.EAST -> -1 to 1
 			}
+			val offset = facing.normal.let { BlockPos((xMine + 1) * it.x, 0, (zMine + 1) * it.z) }
 			var minePos: BlockPos
 			do {
 				if (x > xMine) {
-					x = -10
+					x = -xMine
 					z++
 				}
 				if (z > zMine) {
-					z = -10
+					z = -zMine
 					y--
 				}
-				minePos = BlockPos(this.blockPos.x - (x++ * nX), y, this.blockPos.z - (z * nZ))
+				minePos = BlockPos(
+					(this.blockPos.x - (x++ * nX)) + offset.x,
+					y,
+					(this.blockPos.z - (z * nZ)) + offset.z
+				)
 				val currentState = level.getBlockState(minePos)
 				if (currentState.block == Blocks.BEDROCK) {
 					noUpdate = true
@@ -87,6 +92,13 @@ class QuarryBlockEntity(pPos: BlockPos, pBlockState: BlockState) : BlockEntity(
 		}
 		val drillPos = drillPos!!
 		if (tickTime > 0f) {
+			// TODO: when home: add chunk specific data if possible?
+			// TODO: if not feasible: look at level datastorage
+			level.dataStorage.set("a", object : SavedData() {
+				override fun save(p0: CompoundTag, p1: HolderLookup.Provider): CompoundTag {
+					TODO("Not yet implemented")
+				}
+			})
 			breakingTick++
 			val newProgress = ((breakingTick / tickTime) * 10).roundToInt()
 			if (newProgress != 10) {
